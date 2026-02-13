8e435af2634591743e78414d73da28e685915f2e
Nischal <03seinxera@gmail.com>
2026-02-13 15:33:07 +0545

    added scaling transition for contact link in home page

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -75,17 +75,18 @@ const Home = () => {
         <div className="flex mt-10 gap-2 sm:gap-5 md:gap-7 lg:gap-10">
           <motion.a href="/contact"
             initial={{ opacity: 0, x: -200 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1.2, delay: 3 }}
+            animate={{ opacity: 1, x: 0, transition: {duration: 1.2, delay: 3  } }}
+            whileHover={{ scale: 1.05 , transition: {duration:0.3} }}
             className="text-white bg-[#0D00FC] py-2.5 rounded-xl w-38 sm:text-xl font-semibold sm:w-50 text-center hover:bg-[#0b00dc] transition-colors"
           >
+
             {t("hero.button1")}
           </motion.a>
 
           <motion.a href="/service"
             initial={{ opacity: 0, x: 200 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1.2, delay: 3 }}
+            animate={{ opacity: 1, x: 0 , transition:{duration:1.2, delay:3}}}
+            whileHover={{scale:1.05, transition:{duration:0.3}}}
             className="text-white py-2.5 rounded-xl sm:text-xl w-38 font-semibold sm:w-50 text-center border-white border hover:bg-white/15 transition-colors"
           >
             {t("hero.button2")}
